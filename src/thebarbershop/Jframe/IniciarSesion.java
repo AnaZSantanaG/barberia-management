14b01b9f46119296f39c9a3d68b8ceec2b54a9bd
Ana Z, Santana G. <20242153@itla.edu.do>
2025-08-14 03:20:02 -0400

    iniciar sesion override

--- src/thebarbershop/Jframe/IniciarSesion.java
+++ src/thebarbershop/Jframe/IniciarSesion.java
@@ -277,6 +277,7 @@ public class IniciarSesion extends javax.swing.JFrame {
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
             public void run() {
                 new IniciarSesion().setVisible(true);
             }
